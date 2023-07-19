2e2f844ccfb9403644adeddf9c1b5cb67b3dd9f8
Arame Hovhannisyan <narhov123456@gmail.com>
2023-07-19 16:05:42 +0400

    Creating tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (calculateWinner(board) || board[index]) {
+      return;
+    }
+    const newBoard = [...board];
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div className="App">
+      <div className="board">
+        {board.map((_, index) => (
+          <div key={index} className="square-container">
+            {renderSquare(index)}
+          </div>
+        ))}
+      </div>
+      <div className="status">{status}</div>
+    </div>
+  );
+}
+
+const calculateWinner = (squares) => {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const [a, b, c] of winningLines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+export default App;
